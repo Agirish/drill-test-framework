8e7239ac4ca5ee4697f105a016d5f5b446e88fa3
Abhishek Girish <agirish@mapr.com>
2015-12-07 10:39:40 -0800

    Updating data gen scripts

--- framework/resources/Datasources/tpcds/create.sh
+++ framework/resources/Datasources/tpcds/create.sh
@@ -2,11 +2,13 @@
 
 set -x
 
+source ~/.drillTestConfig
+
 workspace=$1
 path=$2
 script=$3
 
-hadoop fs -test -d $path ;if [ `echo $?` -eq 0 ]; then hadoop fs -rmr $path; hadoop fs -mkdir $path; else hadoop fs -mkdir $path; fi
+$HADOOP_HOME/bin/hadoop fs -test -d $path ;if [ `echo $?` -eq 0 ]; then $HADOOP_HOME/bin/hadoop fs -rm -r $path; $HADOOP_HOME/bin/hadoop fs -mkdir -p $path; else $HADOOP_HOME/bin/hadoop fs -mkdir -p $path; fi
 
 ${DRILL_HOME}/bin/sqlline -n ${USERNAME} -p ${PASSWORD} -u "jdbc:drill:schema=$workspace;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}"  --run=$script
 
